dcc17c8866e59fa88b14a529a53df762297175fe
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 13:17:27 -0500

    write spec for get posts

--- routes/post.js
+++ routes/post.js
@@ -1,31 +1,42 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
+const {} = require('../data/db');
+
 router
-  .use('/')
+  .use("/")
   .get(async (req, res) => {
     // GET posts
+    try {
+      const posts = await find()
+      res.status(200).json(posts);
+    } catch (e) {
+      res.status(500).json({ error: "The posts information could not be retrieved." })
+    }
   })
   .post(async (req, res) => {
     // POST posts
-  })
+  });
 
-router.use('/:id')
-  .get(async ({params: {id}}, res) => {
-    // GET post/id
+router
+  .use("/:id")
+  .get(async ({ params: { id } }, res) => {
+    // GET posts/id
   })
-  .delete(async ({params: {id}}, res) => {
-    // DELETE post/id
+  .delete(async ({ params: { id } }, res) => {
+    // DELETE posts/id
   })
-  .update(async ({params: {id}}, res) => {
-    // UPDATE post/id
-  })
+  .update(async ({ params: { id } }, res) => {
+    // UPDATE posts/id
+  });
 
-router.use('/:id/comments')
-  .get(async ({params: {id}}, res) => {
-    // GET comments of post/id
+router
+  .use("/:id/comments")
+  .get(async ({ params: { id } }, res) => {
+    // GET comments of posts/id
   })
-  .post(async ({params: {id}}, res) => {
-    // post comments to post/id
-  })
+  .post(async ({ params: { id } }, res) => {
+    // post comments to posts/id
+  });
 
+  module.exports.router = router;
