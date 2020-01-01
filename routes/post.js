c2a4835a858a4048c21760bbd27182c7d06130fb
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 13:40:45 -0500

    format comments and multi chained methods

--- routes/post.js
+++ routes/post.js
@@ -1,42 +1,42 @@
 const { Router } = require("express");
 const router = Router();
 
-const {} = require('../data/db');
-
 router
   .use("/")
   .get(async (req, res) => {
-    // GET posts
+    /* GET posts */
     try {
-      const posts = await find()
+      const posts = await find();
       res.status(200).json(posts);
     } catch (e) {
-      res.status(500).json({ error: "The posts information could not be retrieved." })
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
     }
   })
   .post(async (req, res) => {
-    // POST posts
+    /* POST posts */
   });
 
 router
   .use("/:id")
   .get(async ({ params: { id } }, res) => {
-    // GET posts/id
+    /* GET posts/id */
   })
   .delete(async ({ params: { id } }, res) => {
-    // DELETE posts/id
+    /* DELETE posts/id */
   })
   .update(async ({ params: { id } }, res) => {
-    // UPDATE posts/id
+    /* UPDATE posts/id */
   });
 
 router
   .use("/:id/comments")
-  .get(async ({ params: { id } }, res) => {
-    // GET comments of posts/id
+  .get(async (req, res) => {
+    /* GET posts/:id/comments */
   })
-  .post(async ({ params: { id } }, res) => {
-    // post comments to posts/id
+  .post(async (req, res) => {
+    /* POST posts/:id/comments */
   });
 
-  module.exports.router = router;
+module.exports.router = router;
