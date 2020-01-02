a998cf7c4ed0eefedad11f9bbc8f14aaac4bd79b
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 20:52:15 -0500

    import loadPost and use middleware on routes with /:id and destructure req object / remove redundant code

--- routes/post.js
+++ routes/post.js
@@ -12,6 +12,8 @@ const {
   insertComment
 } = require("../data/db");
 
+const loadPost = require('../middleware/loadPost')
+
 router
   .route("/")
   .get(async (_, res) => {
@@ -28,37 +30,47 @@ router
   .post(async ({ body: { title, contents } }, res) => {
     /* POST posts */
     if (!title || !contents) {
-      res
-        .status(400)
-        .json({
-          errorMessage: "Please provide title and contents for the post."
-        });
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+      return;
     }
 
     try {
-      const postFromBody = { title, content };
+      const postFromBody = { title, contents };
 
       const { id } = await insert(postFromBody);
       const post = await findById(id);
       res.status(201).json(post);
     } catch (e) {
-      res
-        .status(500)
-        .json({
-          error: "There was an error while saving the post to the database"
-        });
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
     }
   });
-
+/**
+ * loadPost takes care of the 
+ * find post by id
+ *  send error if post not found
+ * save to req.postData
+ * move to next middleware
+ * 
+ * Will send an error if it could not perform task above as a 500 error
+ */
+router.all('/:id', loadPost)
 router
   .route("/:id")
-  .get(async ({ params: { id } }, res) => {
+  .get(async ({postData: post}, res) => {
     /* GET posts/id */
+    res
+      .status(201)
+      .json(post);
+      return
   })
-  .delete(async ({ params: { id } }, res) => {
+  .delete(async ({postData: post}, res) => {
     /* DELETE posts/id */
   })
-  .put(async ({ params: { id } }, res) => {
+  .put(async ({postData: post}, res) => {
     /* UPDATE posts/id */
   });
 
@@ -67,8 +79,28 @@ router
   .get(async (req, res) => {
     /* GET posts/:id/comments */
   })
-  .post(async (req, res) => {
+  .post(async ({postData: post, body: { text } }, res) => {
     /* POST posts/:id/comments */
+
+    if (!text) {
+      res.status(400).json({
+        errorMessage: "Please provide text for the comment."
+      });
+      return;
+    }
+
+    const commentFromBody = { text, post_id: post.id };
+
+    try {
+      const { id: commentId } = await insertComment(commentFromBody);
+      const comment = await findCommentById(commentId);
+
+      res.status(201).json(comment);
+    } catch (e) {
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database"
+      });
+    }
   });
 
 module.exports.router = router;
