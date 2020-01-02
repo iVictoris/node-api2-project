6eef958bfc7262199d504989cc8621d6a5333ca9
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 19:11:32 -0500

    update path to postRouter

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
-app.use('/posts', postRouter)
+app.use('/api/posts', postRouter)
 
 
 app.listen(port, () => {
