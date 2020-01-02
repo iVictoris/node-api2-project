6918a829b847b8fcba3c3ea3df51b367936d438b
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 19:17:10 -0500

    create post endpoint to create post to api/post with .post method

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,17 @@
 const { Router } = require("express");
 const router = Router();
 
+const {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  findPostComments,
+  findCommentById,
+  insertComment
+} = require("../data/db");
+
 router
   .route("/")
   .get(async (req, res) => {
@@ -14,8 +25,29 @@ router
         .json({ error: "The posts information could not be retrieved." });
     }
   })
-  .post(async (req, res) => {
+  .post(async ({ body: { title, contents } }, res) => {
     /* POST posts */
+    if (!title || !contents) {
+      res
+        .status(400)
+        .json({
+          errorMessage: "Please provide title and contents for the post."
+        });
+    }
+
+    try {
+      const postFromBody = { title, content };
+
+      const { id } = await insert(postFromBody);
+      const post = await findById(id);
+      res.status(201).json(post);
+    } catch (e) {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database"
+        });
+    }
   });
 
 router
