4039954ae477694ae1518db0d43ef944590c0f57
Victor Tran <victor.n.tran@outlook.com>
2020-01-01 19:11:15 -0500

    create basic express application

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const {router: postRouter} = require('./routes/post');
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.use('/posts', postRouter)
+
+
+app.listen(port, () => {
+  console.log(`App is listening to port ${port}`);
+})
